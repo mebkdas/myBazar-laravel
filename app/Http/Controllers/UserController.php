06ac7868c1a38885573d2d95c3a10916d5d0f918
mebkdas <me.bkdas@gmail.com>
2021-12-02 16:09:06 +0530

    login functionality

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\user;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(Request $req){
+        $user = user::where(['email'=>$req->email])->first();
+        if(!$user || !Hash::check($req->password,$user->password)){
+            return "Username or Password not match!!";
+        }
+        else{
+            $req->session()->put('user',$user);
+            return redirect('/');
+        }
+    }
+}
